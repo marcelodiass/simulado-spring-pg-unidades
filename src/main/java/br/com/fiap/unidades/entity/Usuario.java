cc61e9cebf9e8e8f1bb65ecbb93eb2ba203bb4ea
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-04-16 09:01:29 -0300

    Entity Usuario criada

--- src/main/java/br/com/fiap/unidades/entity/Usuario.java
+++ src/main/java/br/com/fiap/unidades/entity/Usuario.java
@@ -1,5 +1,6 @@
 package br.com.fiap.unidades.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,16 +10,33 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
-
+@Table(name = "TB_USUARIO", uniqueConstraints = {
+        @UniqueConstraint(name = "UK_PESSOA", columnNames = "ID_PESSOA")
+})
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_USUARIO")
+    @SequenceGenerator(
+            name = "SQ_USUARIO",
+            sequenceName = "SQ_USUARIO",
+            initialValue = 1,
+            allocationSize = 1
+    )
+    @Column(name = "ID_USUARIO")
     private Long id;
 
+    @Column(name = "USER_USUARIO")
     private String username;
 
+    @Column(name = "PASSWORD_USUARIO")
     private String password;
 
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(
+            name = "PESSOA",
+            referencedColumnName = "ID_PESSOA",
+            foreignKey = @ForeignKey(name = "FK_PESSOA_USUARIO")
+    )
     private Pessoa pessoa;
-
 }
